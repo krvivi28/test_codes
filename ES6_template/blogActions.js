f6b8e912a2ae8d7e01d09d6cb69cd58ad58a1680
vivek <krvivi28@gmail.com>
2023-07-05 23:15:57 +0530

    fixed lec-1 topic-9

--- ES6_template/blogActions.js
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs";
-
-export const writeBlog = (filePath, name) => {
-  fs.appendFileSync(filePath, name);
-};
-export const publishBlog = (filePath) => {
-  return fs.readFileSync(filePath, { encoding: "utf8" });
-};
---
+++ lecture-1/topic-9/problem/index.js
@@ -0,0 +1,6 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+
+// Write your code here
+
+module.exports = server;
