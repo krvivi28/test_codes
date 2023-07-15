3186ced22b33ef2e93cc2126f2dff7a4d8513a05
vivek <krvivi28@gmail.com>
2023-07-16 03:30:21 +0530

    completed lec-9

--- lecture-9/summative/solution/app.test.js
+++ lecture-9/summative/solution/app.test.js
@@ -1,1 +1,18 @@
-// yet to write
+import request from "supertest";
+import app from "./index.js";
+
+describe("GET /api/product", () => {
+  it("The API should be accessible to users who have logged in with JWT.", async () => {
+    const logResp = await request(app)
+      .post("/api/user/login")
+      .send({ email: "krvivi28@gmail.com", password: "vivek28@" });
+    const cookies = logResp.header["set-cookie"];
+    const response = await request(app)
+      .get("/api/product")
+      .set("Cookie", cookies);
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe(
+      '{"success":true,"products":[{"id":1,"name":"iphone","price":10000},{"id":2,"name":"iphone","price":10000},{"id":3,"name":"iphone","price":10000}]}'
+    );
+  });
+});
