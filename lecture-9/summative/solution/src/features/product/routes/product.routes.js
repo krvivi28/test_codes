851d7977ed60aadc061488ce8e75d590f5dba6a9
vivek <krvivi28@gmail.com>
2023-07-16 02:34:58 +0530

    added lec-9 topic-5

---
+++ lecture-9/summative/solution/src/features/product/routes/product.routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  addProduct,
+  getAllProducts,
+  getOneProduct,
+  rateProduct,
+} from "../controller/product.controller.js";
+import authorizer from "../../../middlewares/basicAuth.js";
+import jwtAuth from "../../../middlewares/jwtAuth.js";
+
+
+const router = express.Router();
+
+// get routes
+// router.route("/").get(authorizer, getAllProducts);
+router.route("/").get(jwtAuth, getAllProducts);
+router.route("/:id").get(getOneProduct);
+
+// post routes
+router.route("/").post(addProduct);
+router.route("/rateproduct").get(rateProduct);
+
+export default router;
